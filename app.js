33851e7f156ebb2f7bc9b78f2ca0c99971da6421
David AZOULAY <dazoulay@simplicite.fr>
2015-11-04 17:59:41 +0100

    Upadted

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-app.use(express.static(__dirname + "/public"));
-app.set("view engine", "jade");
-app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
 
-var host = (process.env.VCAP_APP_HOST || "localhost");
+var host = (process.env.VCAP_APP_HOST || 'localhost');
 var port = (process.env.VCAP_APP_PORT || 3000);
 
 var params = {
-	host: process.env.SIMPLICITE_HOST || "demo.apps.simplicite.io",
+	host: process.env.SIMPLICITE_HOST || 'demo.apps.simplicite.io',
 	port: process.env.SIMPLICITE_POST || 80,
-	root: process.env.SIMPLICITE_ROOT || "",
-	user: process.env.SIMPLICITE_USER || "admin",
-	password: process.env.SIMPLICITE_PASSWORD || "admin",
+	root: process.env.SIMPLICITE_ROOT || '',
+	user: process.env.SIMPLICITE_USER || 'admin',
+	password: process.env.SIMPLICITE_PASSWORD || 'admin',
 	debug: true
 };
-var demo = require("simplicite").session(params);
+var demo = require('simplicite').session(params);
 
-var prd = demo.getBusinessObject("DemoProduct");
+var prd = demo.getBusinessObject('DemoProduct');
 
-app.get("/", function(req, res) {
-	console.log("Home page requested");
+app.get('/', function(req, res) {
+	console.log('Home page requested');
 	prd.search(undefined, { inlineDocs: true }).then(function(list) {
-		console.log(list.length + " products loaded !");
-		res.render("index", { products: JSON.stringify(list), });
+		console.log(list.length + ' products loaded !');
+		res.render('index', { products: JSON.stringify(list), });
 	});
 });
 
-app.get("/user", function(req, res) {
-	console.log("User page requested");
+app.get('/user', function(req, res) {
+	console.log('User page requested');
 	demo.getGrant({ inlinePicture: true }).then(function(grant) {
-		res.render("user", { grant: JSON.stringify(grant), });
+		res.render('user', { grant: JSON.stringify(grant), });
 	});
 });
 
-app.get("/health", function(req, res) {
-	console.log("Health page requested");
+app.get('/health', function(req, res) {
+	console.log('Health page requested');
 	demo.getHealth().then(function(health) {
-		res.render("health", health);
+		res.render('health', health);
 	});
 });
 
-console.log("Server listening on " + host + ":" + port);
+console.log('Server listening on ' + host + ':' + port);
 app.listen(port, host);
