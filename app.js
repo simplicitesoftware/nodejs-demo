554c9f6e213278726069b37c79bd1daa14369427
David AZOULAY <dazoulay@simplicite.fr>
2016-06-27 14:43:29 +0200

    Updated

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 "use strict";
 
+function headers(res) {
+	res.header('Cache-Control', 'private, no-cache, no-store, no-transform, must-revalidate');
+	res.header('Expires', '-1');
+	res.header('Pragma', 'no-cache');
+}
+
 var params = {
-	url: process.env.VCAP_SIMPLICITE_URL || 'http://demo.apps.simplicite.io',
-	user: process.env.VCAP_SIMPLICITE_USER || 'admin',
-	password: process.env.VCAP_SIMPLICITE_PASSWORD || 'admin',
+	url: process.env.SIMPLICITE_BASEURL || 'http://demo.apps.simplicite.io',
+	user: process.env.SIMPLICITE_USER || 'admin',
+	password: process.env.SIMPLICITE_PASSWORD || 'admin',
 	debug: true
 };
 var demo = require('simplicite').session(params);
@@ -21,10 +27,8 @@ var serverHost = process.env.VCAP_APP_HOST || args[0] || 'localhost';
 var serverPort = process.env.VCAP_APP_PORT || args[1] || 3000;
 
 app.get('/', function(req, res) {
-	res.header('Cache-Control', 'private, no-cache, no-store, no-transform, must-revalidate');
-	res.header('Expires', '-1');
-	res.header('Pragma', 'no-cache');
 	console.log('Home page requested');
+	headers(res);
 	product.search(undefined, { inlineDocs: true }).then(function(list) {
 		console.log(list.length + ' products loaded !');
 		res.render('index', { products: JSON.stringify(list), });
@@ -32,10 +36,8 @@ app.get('/', function(req, res) {
 });
 
 app.get('/user', function(req, res) {
-	res.header('Cache-Control', 'private, no-cache, no-store, no-transform, must-revalidate');
-	res.header('Expires', '-1');
-	res.header('Pragma', 'no-cache');
 	console.log('User page requested');
+	headers(res);
 	demo.getGrant({ inlinePicture: true }).then(function(grant) {
 		res.render('user', { grant: JSON.stringify(grant), });
 	});
