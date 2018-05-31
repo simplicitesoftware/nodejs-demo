4048f98bdf55bf573cab5228852c2fcfd5f79685
David AZOULAY <dazoulay@simplicite.fr>
2018-05-31 14:02:17 +0200

    Fix

--- public/tools.js
+++ public/tools.js
@@ -7,7 +7,7 @@ function cell(span) {
 }
 
 function card(body, heading, span) {
-	var c = $('<div/>').addClass('card'));
+	var c = $('<div/>').addClass('card');
 	c.append($('<div/>').addClass('card-body')
 		.append($('<h3/>').addClass('card-title').append(heading))
 		.append(body));
