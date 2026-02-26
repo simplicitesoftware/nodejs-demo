4a3b9ad4be153a07c89a0a4c847f229c0b7ac5a5
David AZOULAY <dazoulay@simplicite.fr>
2026-02-26 14:22:27 +0100

    Sonar fixes

--- app.js
+++ app.js
@@ -9,8 +9,8 @@
 
 import simplicite from 'simplicite';
 import express from 'express';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
 
 function headers(res) {
 	res.header('Cache-Control', 'private, no-cache, no-store, no-transform, must-revalidate');
@@ -29,51 +29,50 @@ app.info(`Version: ${app.getModuleVersion()}`);
 app.debug(app.parameters);
 
 try {
-	app.login().then(async user => {
-		app.debug(`Logged in as ${user.login}`);
+	const user = await app.login();
+	app.debug(`Logged in as ${user.login}`);
 
-		const args = process.argv.slice(2);
-		const serverHost = process.env.VCAP_APP_HOST || args[0] || 'localhost';
-		const serverPort = process.env.VCAP_APP_PORT || parseInt(args[1]) || 3000;
+	const args = process.argv.slice(2);
+	const serverHost = process.env.VCAP_APP_HOST || args[0] || 'localhost';
+	const serverPort = process.env.VCAP_APP_PORT || Number.parseInt(args[1]) || 3000;
 
-		const server = express();
-		server.disable('x-powered-by');
-		const dir = dirname(fileURLToPath(import.meta.url));
-		server.use(express.static(`${dir}/public`));
-		server.set('view engine', 'pug');
-		server.set('views', `${dir}/views`);
+	const server = express();
+	server.disable('x-powered-by');
+	const dir = dirname(fileURLToPath(import.meta.url));
+	server.use(express.static(`${dir}/public`));
+	server.set('view engine', 'pug');
+	server.set('views', `${dir}/views`);
 
-		const product = app.getBusinessObject('DemoProduct');
+	const product = app.getBusinessObject('DemoProduct');
 
-		server.get('/', async (req, res) => {
-			app.debug('Home page requested');
-			headers(res);
-			try {
-				const list = await product.search(null, { inlineDocuments: [ 'demoPrdPicture' ] });
-				app.debug(`${list.length} products loaded`);
-				res.render('index', { version: simplicite.constants.MODULE_VERSION, products: JSON.stringify(list) });
-			} catch (err) {
-				app.error(err);
-				res.render('index', { error: err.message });
-			}
-		});
-
-		server.get('/user', async (req, res) => {
-			app.debug('User page requested');
-			headers(res);
-			try {
-				const grant = await app.getGrant({ inlinePicture: true });
-				app.debug(`${grant.login} loaded`);
-				res.render('user', { grant: JSON.stringify(grant) });
-			} catch (err) {
-				app.error(err);
-				res.render('user', { error: err.message });
-			}
-		});
-
-		server.listen(parseInt(serverPort), serverHost);
-		app.info(`Server listening on ${serverHost}:${serverPort}`);
+	server.get('/', async (req, res) => {
+		app.debug('Home page requested');
+		headers(res);
+		try {
+			const list = await product.search(null, { inlineDocuments: [ 'demoPrdPicture' ] });
+			app.debug(`${list.length} products loaded`);
+			res.render('index', { version: simplicite.constants.MODULE_VERSION, products: JSON.stringify(list) });
+		} catch (err) {
+			app.error(err);
+			res.render('index', { error: err.message });
+		}
 	});
+
+	server.get('/user', async (req, res) => {
+		app.debug('User page requested');
+		headers(res);
+		try {
+			const grant = await app.getGrant({ inlinePicture: true });
+			app.debug(`${grant.login} loaded`);
+			res.render('user', { grant: JSON.stringify(grant) });
+		} catch (err) {
+			app.error(err);
+			res.render('user', { error: err.message });
+		}
+	});
+
+	server.listen(Number.parseInt(serverPort), serverHost);
+	app.info(`Server listening on ${serverHost}:${serverPort}`);
 } catch (err) {
 	app.log(err);
 }
